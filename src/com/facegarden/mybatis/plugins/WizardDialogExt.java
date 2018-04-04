83309ba743c8cd66a3706d52205419ea48771d6b
xuqianghui <merlinkael@gmail.com>
2018-04-04 14:28:31 +0800

    重新提交

---
+++ src/com/facegarden/mybatis/plugins/WizardDialogExt.java
@@ -0,0 +1,16 @@
+package com.facegarden.mybatis.plugins;
+
+import org.eclipse.jface.wizard.IWizard;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
+
+public class WizardDialogExt
+  extends WizardDialog
+{
+  public WizardDialogExt(Shell parentShell, IWizard newWizard)
+  {
+    super(parentShell, newWizard);
+    
+    setShellStyle(67680);
+  }
+}
